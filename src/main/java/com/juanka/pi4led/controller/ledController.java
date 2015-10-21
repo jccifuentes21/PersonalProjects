6e6e6eafbe0cbe2ce45c06955764f95c2de59fc8
jccifuentes21 <jccifuentes21@gmail.com>
2015-10-20 22:59:39 -0500

    motor commit

--- src/main/java/com/juanka/pi4led/controller/ledController.java
+++ src/main/java/com/juanka/pi4led/controller/ledController.java
@@ -5,10 +5,8 @@ import com.pi4j.io.gpio.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+/*RestController
 public class ledController {
-
-    private static GpioPinDigitalOutput pin;
 
     @RequestMapping("/")
     public String greeting() {
@@ -67,7 +65,7 @@ public class ledController {
 
             i++;
         }
-
+        pin1.low();
         gpio.shutdown();
 
         gpio.unprovisionPin(pin1);
@@ -80,4 +78,37 @@ public class ledController {
 
     }
 
+}*/
+@RestController
+public class motorController {
+
+    @RequestMapping("/")
+    public String start () {
+
+        return "app is running ok.";
+    }
+
+    @RequestMapping("/drive")
+    public String drive () throws InterruptedException{
+
+        GpioController gpio = GpioFactory.getInstance();
+
+        GpioPinDigitalOutput leftMotor = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "Motor Izquierda", PinState.LOW);
+        GpioPinDigitalOutput rightMotor = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "Motor Derecha", PinState.LOW);
+
+
+        leftMotor.high();
+        rightMotor.high();
+        Thread.sleep(3000);
+
+        gpio.shutdown();
+        gpio.unprovisionPin(leftMotor);
+        gpio.unprovisionPin(rightMotor);
+
+        System.out.println("Motors worked");
+
+        return "Program was executed correctly";
+
+    }
 }
+
